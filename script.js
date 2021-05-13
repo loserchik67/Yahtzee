c9c0b22005eccb47e354baa2c739ddaa67206cad
loserchik67 <loserchik67@gmail.com>
2021-05-13 15:56:05 -0500

    Add functionality for the 1 - 6 buttons

--- script.js
+++ script.js
@@ -14,6 +14,36 @@ const diceEl4 = document.getElementById('dice4img');
 const diceEl5 = document.getElementById('dice5img');
 const startMessage = document.querySelector('.start');
 
+//submit buttons
+const btnOnes = document.getElementById('btnOnes');
+const btnTwos = document.getElementById('btnTwos');
+const btnThrees = document.getElementById('btnThrees');
+const btnFourss = document.getElementById('btnFours');
+const btnFives = document.getElementById('btnFives');
+const btnSixes = document.getElementById('btnSixes');
+
+//score trackers
+let oneScore = 0;
+let twoScore = 0;
+let threeScore = 0;
+let fourScore = 0;
+let fiveScore = 0;
+let sixScore = 0;
+let totalScore = 0;
+let oneSubmit = false;
+let twoSubmit = false;
+let threeSubmit = false;
+let fourSubmit = false;
+let fiveSubmit = false;
+let sixSubmit = false;
+const totalScoreText = document.querySelector('.total-score-text2');
+const onesText = document.getElementById('ones-score');
+const twosText = document.getElementById('twos-score');
+const threesText = document.getElementById('threes-score');
+const foursText = document.getElementById('fours-score');
+const fivesText = document.getElementById('fives-score');
+const sixesText = document.getElementById('sixes-score');
+
 // if it is being held and the value of the dice face
 let rollCount = 0;
 let dice1 = 0;
@@ -28,8 +58,65 @@ let hold4 = false;
 let hold5 = false;
 let playing = true;
 let notStarted = true;
+let hasRolled = false;
+
+//reset hold buttons and dice images
+const unhold = function () {
+  if (rollCount >= 3) {
+    hold1 = false;
+    hold2 = false;
+    hold3 = false;
+    hold4 = false;
+    hold5 = false;
+    diceEl1.src = `dice-${dice1}.png`;
+    diceEl2.src = `dice-${dice2}.png`;
+    diceEl3.src = `dice-${dice3}.png`;
+    diceEl4.src = `dice-${dice4}.png`;
+    diceEl5.src = `dice-${dice5}.png`;
+    btnHold1.textContent = 'hold';
+    btnHold2.textContent = 'hold';
+    btnHold3.textContent = 'hold';
+    btnHold4.textContent = 'hold';
+    btnHold5.textContent = 'hold';
+  }
+};
+
+// after they press submit allow them to make a new roll
+let newRoll = function () {
+  rollCount = 0;
+  hasRolled = false;
+  //reset dice values to relock the hold buttons
+  dice1 = 0;
+  dice2 = 0;
+  dice3 = 0;
+  dice4 = 0;
+  dice5 = 0;
+};
+//start a new game
+btnNewGame.addEventListener('click', function () {
+  oneScore = 0;
+  twoScore = 0;
+  threeScore = 0;
+  fourScore = 0;
+  fiveScore = 0;
+  sixScore = 0;
+  unhold();
+  newRoll();
+  startMessage.classList.remove('hidden');
+  btnNewGame.classList.add('hidden');
+
+  //reset scores
+  totalScoreText.textContent = 0;
+  onesText.textContent = 'Score: -';
+  twosText.textContent = 'Score: -';
+  threesText.textContent = 'Score: -';
+  foursText.textContent = 'Score: -';
+  fivesText.textContent = 'Score: -';
+  sixesText.textContent = 'Score: -';
+});
 
 let roll = function () {
+  hasRolled = true;
   if (rollCount < 3) {
     if (!hold1) {
       dice1 = Math.floor(Math.random() * 6 + 1);
@@ -56,32 +143,15 @@ let roll = function () {
   }
 };
 
-const unhold = function () {
-  if (rollCount >= 3) {
-    hold1 = false;
-    hold2 = false;
-    hold3 = false;
-    hold4 = false;
-    hold5 = false;
-    diceEl1.src = `dice-${dice1}.png`;
-    diceEl2.src = `dice-${dice2}.png`;
-    diceEl3.src = `dice-${dice3}.png`;
-    diceEl4.src = `dice-${dice4}.png`;
-    diceEl5.src = `dice-${dice5}.png`;
-    btnHold1.textContent = 'hold';
-    btnHold2.textContent = 'hold';
-    btnHold3.textContent = 'hold';
-    btnHold4.textContent = 'hold';
-    btnHold5.textContent = 'hold';
-  }
+//create and sort an array
+const makeArray = function () {
+  //populate the array with dice values
+  let diceArray = [dice1, dice2, dice3, dice4, dice5];
+  //sort the array
+  let sortedArray = diceArray.sort();
+  console.log(sortedArray);
+  return sortedArray;
 };
-
-btnNewGame.addEventListener('click', function () {
-  rollCount = 0;
-  unhold();
-  startMessage.classList.remove('hidden');
-  btnNewGame.classList.add('hidden');
-});
 
 btnRoll.addEventListener('click', function () {
   notStarted = false;
@@ -163,5 +233,114 @@ btnHold5.addEventListener('click', function () {
       diceEl5.src = `dice-${dice5}.png`;
       btnHold5.textContent = 'hold';
     }
+  }
+});
+
+//submit buttons
+btnOnes.addEventListener('click', function () {
+  if (!oneSubmit && hasRolled) {
+    oneSubmit = true;
+    const onesArray = makeArray();
+    for (let i = 0; i < onesArray.length; i++) {
+      if (onesArray[i] === 1) {
+        oneScore += 1;
+      }
+    }
+    onesText.textContent = `Score: ${oneScore}`;
+    totalScore += oneScore;
+    totalScoreText.textContent = totalScore;
+    rollCount = 3;
+    newRoll();
+    unhold();
+  }
+});
+
+btnTwos.addEventListener('click', function () {
+  if (!twoSubmit && hasRolled) {
+    twoSubmit = true;
+    const twosArray = makeArray();
+    for (let i = 0; i < twosArray.length; i++) {
+      if (twosArray[i] === 2) {
+        twoScore += 2;
+      }
+    }
+    twosText.textContent = `Score: ${twoScore}`;
+    totalScore += twoScore;
+    totalScoreText.textContent = totalScore;
+    rollCount = 3;
+    newRoll();
+    unhold();
+  }
+});
+
+btnThrees.addEventListener('click', function () {
+  if (!threeSubmit && hasRolled) {
+    threeSubmit = true;
+    const threesArray = makeArray();
+    for (let i = 0; i < threesArray.length; i++) {
+      if (threesArray[i] === 3) {
+        threeScore += 3;
+      }
+    }
+    threesText.textContent = `Score: ${threeScore}`;
+    totalScore += threeScore;
+    totalScoreText.textContent = totalScore;
+    rollCount = 3;
+    newRoll();
+    unhold();
+  }
+});
+
+btnFours.addEventListener('click', function () {
+  if (!fourSubmit && hasRolled) {
+    fourSubmit = true;
+    const foursArray = makeArray();
+    for (let i = 0; i < foursArray.length; i++) {
+      if (foursArray[i] === 4) {
+        fourScore += 4;
+      }
+    }
+    foursText.textContent = `Score: ${fourScore}`;
+    totalScore += fourScore;
+    totalScoreText.textContent = totalScore;
+    rollCount = 3;
+    newRoll();
+    unhold();
+  }
+});
+
+btnFives.addEventListener('click', function () {
+  if (!fiveSubmit && hasRolled) {
+    fiveSubmit = true;
+    const fivesArray = makeArray();
+    for (let i = 0; i < fivesArray.length; i++) {
+      if (fivesArray[i] === 5) {
+        fiveScore += 5;
+      }
+    }
+    fivesText.textContent = `Score: ${fiveScore}`;
+    totalScore += fiveScore;
+    totalScoreText.textContent = totalScore;
+    rollCount = 3;
+    newRoll();
+    unhold();
+  }
+});
+
+btnSixes.addEventListener('click', function () {
+  if (!sixSubmit && hasRolled) {
+    sixSubmit = true;
+    const sixesArray = makeArray();
+    for (let i = 0; i < sixesArray.length; i++) {
+      if (sixesArray[i] === 6) {
+        sixScore += 6;
+      }
+    }
+    sixesText.textContent = `Score: ${sixScore}`;
+    totalScore += sixScore;
+    totalScoreText.textContent = totalScore;
+    rollCount = 3;
+    newRoll();
+    unhold();
   }
 });
